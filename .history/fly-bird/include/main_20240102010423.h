6378f0e4358404589aba1ad2d774fe9e9ce1e3e0
LU Yiqing <128209544+llyiyi@users.noreply.github.com>
2024-01-02 17:10:56 +0800

    Delete .history directory

--- .history/fly-bird/include/main_20240102010423.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_ttf.h"
-#include "SDL2/SDL_mixer.h"
--- .history/fly-bird/src/main_20240102010649.c
+++
@@ -1,8 +0,0 @@
-#include <main.h>
-#undef main
-
-int main()
-{
-    printf("Hello, world!\n");
-    return 0;
-}
